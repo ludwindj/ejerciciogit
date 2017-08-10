391090ea120aec919da653ef61a28c7829770f0d
Ludwin <lrayala@mp.gob.gt>
2017-08-10 16:53:28 -0600

    Agregado a demo2

--- Nuevo documento de texto.java
+++ Nuevo documento de texto.java
@@ -2,35 +2,7 @@ public class Hola{
 	public statitc void main (String arg[]){
 		
 		System.out.println("Otro");
-		System.out.println("Hola mundo, soy un programa");
-		System.out.println("Otro");
 	
-		System.out.println("Otro");
-		System.out.println("Hola mundo, soy un programa");
-		System.out.println("Otro");
-		
-				
-		System.out.println("Otro");
-		System.out.println("Hola mundo, soy un programa");
-		System.out.println("Otro");
-	
-		System.out.println("Otro");
-		System.out.println("Hola mundo, soy un programa");
-		System.out.println("Otro");		
-		System.out.println("Otro");
-		System.out.println("Hola mundo, soy un programa");
-		System.out.println("Otro");
-	
-		System.out.println("Otro");
-		System.out.println("Hola mundo, soy un programa");
-		System.out.println("Otro");		
-		System.out.println("Otro");
-		System.out.println("Hola mundo, soy un programa");
-		System.out.println("Otro");
-	
-		System.out.println("Otro");
-		System.out.println("Hola mundo, soy un programa");
-		System.out.println("Otro");
 		
 	}
 }
