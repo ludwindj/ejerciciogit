69663afe165be635d6139f84a015fe514a30442b
Ludwin <lrayala@mp.gob.gt>
2017-08-10 16:40:08 -0600

    cambios

--- Nuevo documento de texto.java
+++ Nuevo documento de texto.java
@@ -1,5 +1,11 @@
 public class Hola{
 	public statitc void main (String arg[]){
+		
+		System.out.println("Otro");
+		System.out.println("Hola mundo, soy un programa");
+		System.out.println("Otro");
+	}
+		System.out.println("Otro");
 		System.out.println("Hola mundo, soy un programa");
 		System.out.println("Otro");
 	}
