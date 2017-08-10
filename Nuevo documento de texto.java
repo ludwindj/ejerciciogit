9417db661c3b71f0218f763f76adfe7d5e53c8db
Ludwin <lrayala@mp.gob.gt>
2017-08-10 17:51:05 -0600

    sadfsadf

--- Nuevo documento de texto.java
+++ Nuevo documento de texto.java
@@ -1,10 +1,6 @@
 public class Hola{
 	public statitc void main (String arg[]){
-		
-		System.out.println("Otro");
-		System.out.println("Hola mundo, soy un programa");
-		System.out.println("Otro");
-	}
+		sdfsadfsadf	
 		System.out.println("Otro");
 		System.out.println("Hola mundo, soy un programa");
 		System.out.println("Otro");
