0c69e40458600ac2dd3028f254d9412eaf15ca0c
Ludwin <lrayala@mp.gob.gt>
2017-08-10 18:05:30 -0600

    asdfasdf

--- Nuevo documento de texto.java
+++ Nuevo documento de texto.java
@@ -1,6 +1,6 @@
-public class Hola{
+adfaublic class Hola{
 	public statitc void main (String arg[]){
-		sdfsadfsadf	
+		adffdsadfsdafdfsadfsadf	
 		System.out.println("Otro");
 		System.out.println("Hola mundo, soy un programa");
 		System.out.println("Otro");
